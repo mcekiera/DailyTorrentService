fe8d25beaafa12b154bb0078449b85b053960ec9
mcekiera <marcin.cekiera@gmail.com>
2017-01-31 12:08:13 +0100

    XmlMovieData source and processor

--- src/main/java/pl/mcekiera/model/RawXmlMovieData.java
+++ src/main/java/pl/mcekiera/model/RawXmlMovieData.java
@@ -1,4 +1,29 @@
 package pl.mcekiera.model;
 
+import java.util.Date;
+
 public class RawXmlMovieData {
+    private final String title;
+    private final String link;
+    private final String publicationDate;
+
+    public RawXmlMovieData(String title, String link, String date) {
+        this.title = title;
+        this.link = link;
+        this.publicationDate = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+
 }
--- src/main/java/pl/mcekiera/service/XmlMovieDataProcessor.java
+++ src/main/java/pl/mcekiera/service/XmlMovieDataProcessor.java
@@ -1,16 +1,51 @@
 package pl.mcekiera.service;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import pl.mcekiera.model.RawXmlMovieData;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class XmlMovieDataProcessor {
+    private List<RawXmlMovieData> data;
 
-    public boolean fetchData(String url) {
-        return false;
+    public boolean fetchData(String rssUrl) {
+
+        try {
+            URL url = new URL(rssUrl);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = db = dbf.newDocumentBuilder();
+            Document xmlDoc = db.parse(url.openStream());
+            data = parseData(xmlDoc);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+
     }
 
     public List<RawXmlMovieData> getRawXmlMovieData() {
         return null;
+    }
+
+    private List<RawXmlMovieData> parseData(Document doc) {
+        NodeList list = doc.getElementsByTagName("item");
+        List<RawXmlMovieData> data = new ArrayList<>();
+
+        for (int i = 0; i < list.getLength(); i++) {
+            String title = list.item(i).getChildNodes().item(1).getTextContent();
+            String link = list.item(i).getChildNodes().item(3).getTextContent();
+            String date = list.item(i).getChildNodes().item(5).getTextContent();
+            RawXmlMovieData movie = new RawXmlMovieData(title, link, date);
+            data.add(movie);
+        }
+
+        return data;
     }
 }
