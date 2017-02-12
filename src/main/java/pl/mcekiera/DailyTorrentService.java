1e6bd399b31192645981dd9d255a47939d63ab32
mcekiera <marcin.cekiera@gmail.com>
2017-02-12 15:59:23 +0100

    working basic spring boot ver

---
+++ src/main/java/pl/mcekiera/DailyTorrentService.java
@@ -0,0 +1,13 @@
+package pl.mcekiera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTorrentService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTorrentService.class, args);
+    }
+
+}
