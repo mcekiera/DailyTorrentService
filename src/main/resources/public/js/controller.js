e0a06b74dc4f3c9ba9995c6b698d6ace1e4f004c
mcekiera <marcin.cekiera@gmail.com>
2017-02-24 19:04:53 +0100

    frontend: form submit

--- src/main/resources/public/js/controller.js
+++ src/main/resources/public/js/controller.js
@@ -1,7 +1,80 @@
+Ractive.DEBUG = false;
 var daily_torrent = function() {
 
+	function submitCheck() {
+		var check = inputEmail.get('valid') && inputRating.get('valid') && inputRadio.get('valid');
+		formButton.set('enabled', check);
+	}
 
-    var arr = [];
+	function clear() {
+		$('input:radio').each(function () {
+			$(this).prop('checked', false);
+		});
+		$('input:text').each(function () {
+			$(this).value = "";
+		})
+	}
+
+	function collectData() {
+		var id = inputEmail.get('id');
+		var rating = inputRating.get('rating');
+		var whitelist = [];
+		var blacklist = [];
+
+		$('input:radio:checked').each(function () {
+			if($(this).hasClass('whitelist')) {
+				whitelist.push($(this).val());
+			} else if($(this).hasClass('blacklist')) {
+				blacklist.push($(this).val());
+			}
+			console.log($(this).className)
+		});
+
+		console.log({
+			'id': id,
+			'rating': rating,
+			'whitelist': whitelist,
+			'blacklist': blacklist
+		});
+	}
+
+	function registerUser() {
+		$.post('/api/profiles/',collectData(),function () {
+			alert('success');
+		})
+	}
+
+    var InputRactive = Ractive.extend({
+        data: {
+            valid: false
+        },
+
+        regex: /.*/,
+        inputEl: null,
+        verify: function () {
+            this.set('valid', this.regex.test(this.inputEl.value));
+			submitCheck();
+        }
+    });
+
+    var inputEmail = new InputRactive({
+        el: '#input-email-ractive',
+        template: '#input-email-template',
+        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		data: {
+			id: ''
+		}
+    });
+
+    var inputRating = new InputRactive({
+        el: '#input-rating-ractive',
+        template: '#input-rating-template',
+        regex: /^(10|\d(\.\d)?)$/,
+		data: {
+			rating: '',
+			valid: false
+		}
+    });
 
     var dbTable = new Ractive({
         el: '#db-table-ractive',
@@ -9,90 +82,57 @@ var daily_torrent = function() {
         data: {
             that: this,
             valid: false,
-            movies: arr,
+            movies: [],
             isLoaded: false,
-            formatToQuery: function (title) {
-                return title.replace(/\s+/g, '+');
-            },
             op: 0
         }
 
     });
 
-    var inputEmail = new Ractive({
-        el: '#input-email-ractive',
-        template: '#input-email-template',
-        data: {
-            valid: false
-        },
-
-        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        inputEl: this.find("#input-email-element"),
-        verify: function () {
-            this.set('valid', this.regex.test(this.inputEl.value));
-        }
-    });
-
-    var inputRating = new Ractive({
-        el: '#input-rating-ractive',
-        template: '#input-rating-template',
-        data: {
-            valid: false
-        },
-
-        regex: /10|\d(\.\d)?/,
-        inputEl: null,
-        verify: function () {
-            this.set('valid', this.regex.test(this.inputEl.value));
-        }
-    });
-
     var inputRadio = new Ractive({
         el: '#input-radio-ractive',
         template: '#input-radio-template',
+		verify: function () {
+			if($('.whitelist:checked').length > 0) {
+				this.set('valid', true);
+			} else {
+				this.set('valid', false)
+			}
+			submitCheck();
+		},
         data: {
             lists: ["Whitelist","Blacklist"].sort(),
             genres: ['Drama', 'Comedy', 'Action', 'Romance', 'Thriller',
                 'Animation', 'Family', 'Horror', 'Music', 'Adventure',
                 'Fantasy', 'Sci-Fi', 'Mystery', 'Biography', 'Sport',
-                'History', 'Musical', 'Western', 'War', 'Film-Noir'].sort()
-        },
-
-        clearRadio: clear,
-        register: function () {
-            
+                'History', 'Musical', 'Western', 'War', 'Film-Noir'].sort(),
+			valid: false
         }
     });
+
+	var formButton = new Ractive({
+		el: '#form-buttons-ractive',
+		template: '#form-buttons-template',
+		data: {
+			active: false,
+			enabled: false
+		},
+
+		clearRadio: clear,
+		register: registerUser
+	});
 
     inputRating.inputEl = inputRating.find("#input-rating-element");
     inputEmail.inputEl = inputEmail.find("#input-email-element");
 
-    function clear() {
-        $('input:radio').each(function () {
-            $(this).prop('checked', false);
-        });
-        $('input:text').each(function () {
-            $(this).value = "";
-        })
-    };
-
-    var getMovies = function () {
+    (function () {
 
         $.getJSON("/api/", function (data) {
-            dbTable.set('movies', data)
+            dbTable.set('movies', data);
+			dbTable.set("isLoaded", true);
         });
-    };
+    })();
 
-    setTimeout(function () {
-        dbTable.set("isLoaded", true);
-        dbTable.animate("op", 1, {
-            easing: 'easeOut',
-            duration: 3000
-        });
-    }, 5000)
-
-
-    getMovies();
     clear();
 };
 
