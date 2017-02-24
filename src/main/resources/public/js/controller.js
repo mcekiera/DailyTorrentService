bef28b07f46f0a41af5b018e57bc616c5f3a2f11
mcekiera <marcin.cekiera@gmail.com>
2017-02-24 15:33:22 +0100

    frontend: input radio ractive

--- src/main/resources/public/js/controller.js
+++ src/main/resources/public/js/controller.js
@@ -1,80 +1,101 @@
-var url = "localhost:8080/api/"
+var daily_torrent = function() {
 
 
-var arr = [];
+    var arr = [];
 
-var table = new Ractive({
-    el: '#main',
-    template: '#dbTable',
-    onInput: function () {
-        console.log(/^\d+$/.test(document.getElementById("emailInput2").value));
-        this.set('valid',/^\d+$/.test(document.getElementById("emailInput2").value));
-    },
-    data: {
-        that: this,
-        valid: false,
-        movies: arr,
-        isLoaded: false,
-        formatToQuery: function (title) {
-            return title.replace(/\s+/g,'+');
+    var dbTable = new Ractive({
+        el: '#db-table-ractive',
+        template: '#db-table-template',
+        data: {
+            that: this,
+            valid: false,
+            movies: arr,
+            isLoaded: false,
+            formatToQuery: function (title) {
+                return title.replace(/\s+/g, '+');
+            },
+            op: 0
+        }
+
+    });
+
+    var inputEmail = new Ractive({
+        el: '#input-email-ractive',
+        template: '#input-email-template',
+        data: {
+            valid: false
         },
-        op: 0
-    }
 
-});
-
-var inputEmail = new Ractive({
-    el: '#input-email-ractive',
-    template: '#input-email-template',
-    data: {
-        valid: false
-    },
-
-    regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    inputEl: null,
-    verify: function(){
-        this.set('valid',this.regex.test(this.inputEl.value));
-    }
-});
-
-inputEmail.inputEl = document.getElementById('input-email-element');
-
-var inputRating = new Ractive({
-    el: '#input-rating-ractive',
-    template: '#input-rating-template',
-    data: {
-        valid: false
-    },
-
-    regex: /10|\d(\.\d)?/,
-    inputEl: null,
-    verify: function(){
-        this.set('valid',this.regex.test(this.inputEl.value));
-    }
-});
-
-inputEmail.inputEl = document.getElementById('input-email-element');
-inputRating.inputEl = document.getElementById('input-rating-element');
-
-
-
-
-var getMovies = function() {
-
-    $.getJSON("/api/", function (data) {
-        table.set('movies', data)
+        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        inputEl: this.find("#input-email-element"),
+        verify: function () {
+            this.set('valid', this.regex.test(this.inputEl.value));
+        }
     });
-} ;
 
-setTimeout(function () {
-    table.set("isLoaded", true);
-    table.animate("op",1,{
-        easing: 'easeOut',
-        duration: 3000
+    var inputRating = new Ractive({
+        el: '#input-rating-ractive',
+        template: '#input-rating-template',
+        data: {
+            valid: false
+        },
+
+        regex: /10|\d(\.\d)?/,
+        inputEl: null,
+        verify: function () {
+            this.set('valid', this.regex.test(this.inputEl.value));
+        }
     });
-},5000)
+
+    var inputRadio = new Ractive({
+        el: '#input-radio-ractive',
+        template: '#input-radio-template',
+        data: {
+            lists: ["Whitelist","Blacklist"].sort(),
+            genres: ['Drama', 'Comedy', 'Action', 'Romance', 'Thriller',
+                'Animation', 'Family', 'Horror', 'Music', 'Adventure',
+                'Fantasy', 'Sci-Fi', 'Mystery', 'Biography', 'Sport',
+                'History', 'Musical', 'Western', 'War', 'Film-Noir'].sort()
+        },
+
+        clearRadio: clear,
+        register: function () {
+            
+        }
+    });
+
+    inputRating.inputEl = inputRating.find("#input-rating-element");
+    inputEmail.inputEl = inputEmail.find("#input-email-element");
+
+    function clear() {
+        $('input:radio').each(function () {
+            $(this).prop('checked', false);
+        });
+        $('input:text').each(function () {
+            $(this).value = "";
+        })
+    };
+
+    var getMovies = function () {
+
+        $.getJSON("/api/", function (data) {
+            dbTable.set('movies', data)
+        });
+    };
+
+    setTimeout(function () {
+        dbTable.set("isLoaded", true);
+        dbTable.animate("op", 1, {
+            easing: 'easeOut',
+            duration: 3000
+        });
+    }, 5000)
 
 
-getMovies();
+    getMovies();
+    clear();
+};
+
+daily_torrent();
 
 
